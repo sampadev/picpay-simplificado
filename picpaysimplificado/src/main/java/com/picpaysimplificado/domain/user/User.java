b48f455d19b0eca033384ccec71df1185aab356f
sampadev <elivelton.sampaio@hotmail.com>
2025-07-16 12:36:28 -0300

    feat: Update User Class

--- picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,25 +1,18 @@
 package com.picpaysimplificado.domain.user;
 
 import jakarta.persistence.*;
-import lombok.*;
-
 import java.math.BigDecimal;
+import java.util.Objects;
 
-@Entity(name="users")
+@Entity(name = "users")
 @Table(name = "users")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of="id")
-
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    String firstName;
-
+    private String firstName;
     private String lastName;
 
     @Column(unique = true)
@@ -35,4 +28,100 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+    // Construtor vazio (obrigatório para JPA)
+    public User() {
+    }
+
+    // Construtor completo
+    public User(long id, String firstName, String lastName, String document, String email, String password, BigDecimal balance, UserType userType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.userType = userType;
+    }
+
+    // Getters e Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    // equals() e hashCode() baseado no campo "id"
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
