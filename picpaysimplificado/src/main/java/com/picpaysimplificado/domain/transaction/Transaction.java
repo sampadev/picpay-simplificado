ab4da53cbf6229cdd79293a58d0b1301a802bac7
sampadev <elivelton.sampaio@hotmail.com>
2025-07-23 14:23:43 -0300

    feat: Update Transaction.java

--- picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -1,35 +1,85 @@
 package com.picpaysimplificado.domain.transaction;
 
-
 import com.picpaysimplificado.domain.user.User;
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity(name="transaction")
-@Table(name="transaction")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of="id")
-
+@Entity(name = "transaction")
+@Table(name = "transaction")
 public class Transaction {
+
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private BigDecimal amount;
 
     @ManyToOne
-    @JoinColumn(name="sender_id")
+    @JoinColumn(name = "sender_id")
     private User sender;
 
     @ManyToOne
-    @JoinColumn(name="receiver_id")
+    @JoinColumn(name = "receiver_id")
     private User receiver;
 
     private LocalDateTime timestamp;
+
+    public Transaction() {}
+
+    public Transaction(Long id, BigDecimal amount, User sender, User receiver, LocalDateTime timestamp) {
+        this.id = id;
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = timestamp;
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
 }
