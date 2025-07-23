0ec5fbc9f5ed8248a0b287344aa7e910f3f4eda2
sampadev <elivelton.sampaio@hotmail.com>
2025-07-23 14:26:24 -0300

    feat: Implementation UserService.java

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,44 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuario do tipo Logista nao esta autorizado a realizar transacao");
+        }
+        if(sender.getBalance().compareTo(amount)<0){
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findById(id).orElseThrow(() -> new Exception("Usuario nao encontrado"));
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
